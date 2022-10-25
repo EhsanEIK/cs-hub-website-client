c5f2d6f3cb03171ee9573e713002df8ab0731bb7
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-25 13:45:22 +0600

    added modal on checkout page after confirming the purchase

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Checkout = () => {
+    const { user } = useContext(AuthContext);
     const course = useLoaderData();
     const { title, price } = course;
 
@@ -15,19 +17,35 @@ const Checkout = () => {
                 <div className="bg-base-100 rounded-xl shadow-2xl p-10">
                     <div>
                         <p className='text-lg font-bold mb-2'>
-                            Price: <span className='text-red-600'>{price}</span></p>
+                            Price: <span className='text-red-600'>{price}$</span></p>
                         <p className='text-lg font-bold mb-2'>
-                            Vat: <span className='text-red-600'>{vat}</span></p>
+                            Vat: <span className='text-red-600'>{vat}$</span></p>
                         <p className='text-lg font-bold mb-2'>
-                            Total Price: <span className='text-red-600'>{totalPrice}</span></p>
+                            Total Price: <span className='text-red-600'>{totalPrice}$</span></p>
                     </div>
 
                     <div className="mt-6">
-                        <button className="w-full btn btn-primary">Checkout Confirm</button>
+                        <label htmlFor="my-modal" className="modal-button w-full btn btn-primary">Checkout Confirm</label>
                     </div>
+
+                    {/* modal start */}
+                    <input type="checkbox" id="my-modal" className="modal-toggle" />
+                    <div className="modal">
+                        <div className="modal-box">
+                            <h3 className="font-bold text-lg">Congratulations <span className='text-cyan-800'>{user?.displayName}</span></h3>
+                            <p className="pt-4">Your purchase has been successful.
+                            </p>
+                            <p>The course you are enrolled for <span className='font-bold'>{title}</span>.
+                            </p>
+                            <div className="modal-action">
+                                <label htmlFor="my-modal" className="btn">Yay!</label>
+                            </div>
+                        </div>
+                    </div>
+                    {/* modal end */}
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
