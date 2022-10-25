936ec39bbc778ad43c3410f7900fc613e60bcb4e
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-25 18:51:54 +0600

    added footer

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-gray-800 text-white text-center fixed w-full bottom-0 p-2'>
+            <p className='container mx-auto'>&copy; 2022 - Developed by <a href="https://www.linkedin.com/in/ehsanulislamkhan/" target="_blank" rel="noopener noreferrer" className="text-warning text-decoration-none">Ehsan</a></p>
+        </div>
+    );
+};
+
+export default Footer;
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Shared/Footer/Footer';
 import Header from '../Pages/Shared/Header/Header';
 
 const Main = () => {
     return (
-        <div className='container mx-auto'>
+        <div>
             <Header></Header>
-            <Outlet></Outlet>
+            <div className='container mx-auto mb-20'>
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
